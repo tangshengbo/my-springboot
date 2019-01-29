3854a60165ec1d0902e61b4a7a35bc51505ef188
Tangshengbo <867850520tangbo@sina.com>
2019-01-29 18:57:53 +0800

    add Access-Control-Allow-Origin code

--- src/main/java/com/tangshengbo/controller/UserController.java
+++ src/main/java/com/tangshengbo/controller/UserController.java
@@ -9,9 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.List;
 
 /**
@@ -49,13 +49,15 @@ public class UserController {
     }
 
     @GetMapping("/jsonp")
-    public String jsonpCall(@RequestParam("callbackParam") String callback) {
-        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        requestAttributes.getResponse().setHeader("Access-Control-Allow-Origin", "*");
-
+    public void jsonpCall(@RequestParam(value = "callback", required = false) String callback, HttpServletResponse response) throws Exception {
+//        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//        requestAttributes.getResponse().setHeader("Access-Control-Allow-Origin", "http://localhost:8085");DispatcherServlet
         logger.info("jsonpCall param:{}", callback);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("jsonp", "成功");
-        return callback + "()";
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.write(callback + "(" +jsonObject.toJSONString() + ")");
+        writer.flush();
     }
 }
