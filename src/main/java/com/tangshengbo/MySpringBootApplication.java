75075922d1884284e42b7fca33b19ae8e6337bc0
Tangshengbo <867850520tangbo@sina.com>
2019-03-19 18:57:30 +0800

    add swagger code

--- src/main/java/com/tangshengbo/MySpringBootApplication.java
+++ src/main/java/com/tangshengbo/MySpringBootApplication.java
@@ -1,13 +1,14 @@
 package com.tangshengbo;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSwagger2Doc
 @SpringBootApplication
 @MapperScan("com.tangshengbo.dao")
 public class MySpringBootApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBootApplication.class, args);
